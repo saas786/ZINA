9dff9a93c03da5ab2fe72050396c192e5c1550ff
Andreis Purim <andreispurim@gmail.com>
2020-11-10 18:33:26 -0300

    Updates

---
+++ precache-manifest.1e523d3537910092cf91976dcb79752b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ac2a5668bde0d453d27f8ffbea01b8",
+    "url": "/ZINA/index.html"
+  },
+  {
+    "revision": "af481bede0a71f6871e4",
+    "url": "/ZINA/static/css/2.430af4cd.chunk.css"
+  },
+  {
+    "revision": "2f74270b8f791129c30d",
+    "url": "/ZINA/static/css/main.a2cbbfff.chunk.css"
+  },
+  {
+    "revision": "af481bede0a71f6871e4",
+    "url": "/ZINA/static/js/2.8331e8ff.chunk.js"
+  },
+  {
+    "revision": "2f74270b8f791129c30d",
+    "url": "/ZINA/static/js/main.b99f8396.chunk.js"
+  },
+  {
+    "revision": "6774f5db9040ddb924dd",
+    "url": "/ZINA/static/js/runtime~main.62fad1d6.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/ZINA/static/media/venue_location_icon.2ceaf661.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/familytree/precache-manifest.853d7167d0b9403693737bad9617c61f.js"
+  "/ZINA/precache-manifest.1e523d3537910092cf91976dcb79752b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/familytree/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZINA/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
